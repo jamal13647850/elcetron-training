c3b8a33ca7a5bf734aca90989ee2f451628c454d
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-07-02 23:34:23 +0430

    Install and config devtron

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ import path from 'path';
 
 import electronReload from 'electron-reload';
 electronReload(__dirname);
+
+import Devtron from 'devtron';
+
+
 app.on('ready', () => {
+    Devtron.install();
+
     let mainWin = new BrowserWindow({width:800,height:600});
     mainWin.loadURL(url.format({
         pathname : path.join(__dirname,'index.html'),
