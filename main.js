53374fc6e33719baf49a7512a81046fe5c6e4539
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-07-03 00:08:53 +0430

    Some changes by BrowserWindow

--- main.js
+++ main.js
@@ -14,9 +14,19 @@ import Devtron from 'devtron';
 app.on('ready', () => {
     Devtron.install();
 
-    let mainWin = new BrowserWindow({width:800,height:600});
-    mainWin.loadURL(url.format({
+    //let mainWin = new BrowserWindow({width:800,height:600,show : false});
+    let mainWin = new BrowserWindow({width:1200,height:600,backgroundColor : '#27ae60'});
+    let childwin = new BrowserWindow({width:800,height:600,backgroundColor : '#d35400',parent: mainWin});
+   /* mainWin.loadURL(url.format({
         pathname : path.join(__dirname,'index.html'),
         protocol : 'file'
-    }))
+    }));*/
+    mainWin.loadURL('https://gosafir.com');
+    childwin.loadURL(`file://${__dirname}/index.html`);
+    /*mainWin.once('ready-to-show',()=>{
+        mainWin.show();
+    })*/
+    childwin.on('blur',()=>{
+        console.log("blur");
+    })
 });
