059c626497e97066a8e0a9826e09c14418795d23
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-07-02 23:14:21 +0430

    install and config babeljs

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const {app , BrowserWindow} = require('electron');
-const url = require('url');
-const path = require('path');
+//const {app , BrowserWindow} = require('electron');
+import {app , BrowserWindow} from 'electron';
+//const url = require('url');
+import url from 'url';
+//const path = require('path');
+import path from 'path';
 
 app.on('ready', () => {
     let mainWin = new BrowserWindow({width:800,height:600});
